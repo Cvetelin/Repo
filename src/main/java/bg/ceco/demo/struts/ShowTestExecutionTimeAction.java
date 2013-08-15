f581b8e9bd884da3c2ea0762387ec51abac89dd2
bengyzara5@gmail.com <bengyzara5@gmail.com@14d19f41-e975-8858-f4bc-0f3b985422cf>
2013-08-15 14:47:36 +0000

    Spring mvc version start. Major modifications

--- src/main/java/bg/ceco/demo/struts/ShowTestExecutionTimeAction.java
+++ src/main/java/bg/ceco/demo/struts/ShowTestExecutionTimeAction.java
@@ -1,66 +1,66 @@
-package bg.ceco.demo.struts;
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.io.FileUtils;
-import org.apache.struts.action.Action;
-import org.apache.struts.action.ActionForm;
-import org.apache.struts.action.ActionForward;
-import org.apache.struts.action.ActionMapping;
-import org.apache.struts.actions.DispatchAction;
-
-public class ShowTestExecutionTimeAction extends DispatchAction {
-
-	private static final String FORWARD_SUCCESS = "success";
-
-	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		String path = request.getParameter("path");
-		String name = request.getParameter("name");
-		request.setAttribute("testExecutions", getTestExecutions(path));
-		return mapping.findForward(FORWARD_SUCCESS);
-	}
-
-	protected List<DirInfo> getTestExecutions(String pathToDir) throws Exception {
-
-		List<DirInfo> dirInfos = new ArrayList<DirInfo>();
-		File dir = new File(pathToDir);
-		for (File child : dirListByAscendingDate(dir)) {
-
-			if (".".equals(child.getName()) || "..".equals(child.getName())) {
-				continue; // Ignore the self and parent aliases.\
-			}
-			DirInfo bean = new DirInfo();
-			bean.setName(child.getName());
-			bean.setPath(child.getCanonicalPath());
-			dirInfos.add(bean);
-		}
-		return dirInfos;
-	}
-	
-	public ActionForward deleteGallery(ActionMapping mapping, ActionForm form, HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		String path = request.getParameter("path");
-		File folder = new File(path);
-		FileUtils.forceDelete(folder);
-		return mapping.findForward(FORWARD_SUCCESS);
-	}
-	@SuppressWarnings("unchecked")
-	protected static File[] dirListByAscendingDate(File folder) {
-		if (!folder.isDirectory()) {
-			return null;
-		}
-		File[] files = folder.listFiles();
-		Arrays.sort(files, new Comparator() {
-			public int compare(final Object o1, final Object o2) {
-				return new Long(((File) o1).lastModified()).compareTo(new Long(((File) o2).lastModified()));
-			}
-		});
-		return files;
-	}
-}
+//package bg.ceco.demo.struts;
+//import java.io.File;
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Comparator;
+//import java.util.List;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.apache.commons.io.FileUtils;
+//import org.apache.struts.action.Action;
+//import org.apache.struts.action.ActionForm;
+//import org.apache.struts.action.ActionForward;
+//import org.apache.struts.action.ActionMapping;
+//import org.apache.struts.actions.DispatchAction;
+//
+//public class ShowTestExecutionTimeAction extends DispatchAction {
+//
+//	private static final String FORWARD_SUCCESS = "success";
+//
+//	public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+//			HttpServletResponse response) throws Exception {
+//		String path = request.getParameter("path");
+//		String name = request.getParameter("name");
+//		request.setAttribute("testExecutions", getTestExecutions(path));
+//		return mapping.findForward(FORWARD_SUCCESS);
+//	}
+//
+//	protected List<DirInfo> getTestExecutions(String pathToDir) throws Exception {
+//
+//		List<DirInfo> dirInfos = new ArrayList<DirInfo>();
+//		File dir = new File(pathToDir);
+//		for (File child : dirListByAscendingDate(dir)) {
+//
+//			if (".".equals(child.getName()) || "..".equals(child.getName())) {
+//				continue; // Ignore the self and parent aliases.\
+//			}
+//			DirInfo bean = new DirInfo();
+//			bean.setName(child.getName());
+//			bean.setPath(child.getCanonicalPath());
+//			dirInfos.add(bean);
+//		}
+//		return dirInfos;
+//	}
+//	
+//	public ActionForward deleteGallery(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+//			HttpServletResponse response) throws Exception {
+//		String path = request.getParameter("path");
+//		File folder = new File(path);
+//		FileUtils.forceDelete(folder);
+//		return mapping.findForward(FORWARD_SUCCESS);
+//	}
+//	@SuppressWarnings("unchecked")
+//	protected static File[] dirListByAscendingDate(File folder) {
+//		if (!folder.isDirectory()) {
+//			return null;
+//		}
+//		File[] files = folder.listFiles();
+//		Arrays.sort(files, new Comparator() {
+//			public int compare(final Object o1, final Object o2) {
+//				return new Long(((File) o1).lastModified()).compareTo(new Long(((File) o2).lastModified()));
+//			}
+//		});
+//		return files;
+//	}
+//}
