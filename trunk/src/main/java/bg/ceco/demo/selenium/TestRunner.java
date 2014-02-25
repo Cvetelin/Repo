0a7321196c5de5277a2bc9931b5ce797a9b8335f
bengyzara5@gmail.com <bengyzara5@gmail.com@14d19f41-e975-8858-f4bc-0f3b985422cf>
2014-02-25 16:28:01 +0000

    Modifications

---
+++ trunk/src/main/java/bg/ceco/demo/selenium/TestRunner.java
@@ -0,0 +1,10 @@
+package bg.ceco.demo.selenium;
+
+public class TestRunner {
+
+//	public static void bla(String[] args) {
+//		JUnitCore runner = new JUnitCore();
+//		runner.addListener(new TestListener());
+//		runner.run(ProjectsIntegrationTest.class);
+//	}
+}
